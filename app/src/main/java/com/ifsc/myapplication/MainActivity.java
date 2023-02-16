e8b40d577ae592bb3ad0d3d00fceadbeddcb863a
Juliana Stefanes Decarlli <julisd3@gmail.com>
2023-02-16 18:29:07 -0300

    add sorteio

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -3,26 +3,37 @@ package com.ifsc.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int contador;
+
+    EditText edmin;
+    EditText edmax;
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contador=0;
         textView=findViewById(R.id.textView);
-        textView.setText(Integer.toString(contador));
+        edmin=findViewById(R.id.edMin);
+        edmax = findViewById(R.id.edMax);
     }
 
-    public void contaClick(View view){
-        contador++;
-        textView.setText(Integer.toString(contador));
+    public void sorteioNumero(View v){
+        int rand = sortRandomIntervalo(
+                Integer.parseInt(edmin.getText().toString()),
+                Integer.parseInt(edmax.getText().toString()));
 
+        textView.setText(Integer.toString(rand));
 
     }
+
+    public int sortRandomIntervalo(int min, int max){
+        return (int) (Math.random()*(max-min +1)+min);
+    }
+
 }
+
